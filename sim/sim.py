a0b98b0ff25ea77d323c8098ab0f7d4b22346047
ren <hust_ren@hust.edu.cn>
2018-11-19 09:52:44 +0800

    无

--- sim/sim.py
+++ sim/sim.py
@@ -12,15 +12,15 @@ def main():
     targetPath = 'D:/LaserData/plane/plane1.png'
     scrPath    = 'D:/LaserData/background/320X256/1.png'
     laserPath  = 'D:/LaserData/LaserSpot/' + group + '.bmp'
-    ansPath    = 'D:/LaserData/ans/' + group + '/'
+    ansPath    = 'D:/LaserData/ans/' + 'template' + '/'
     target = cv2.imread(targetPath, cv2.IMREAD_GRAYSCALE)
     scr    = cv2.imread(scrPath, cv2.IMREAD_GRAYSCALE)
     laser  = cv2.imread(laserPath, cv2.IMREAD_GRAYSCALE)   
     for angle in range(0, 360, 45):
         for r in range(0, 120, 20):
-            filename = group + '_' + str(angle) + '_' + str(r) + '.png'
+            filename = 'template' + '_' + str(angle) + '_' + str(r) + '.png'
             print(filename)
-            simImg   = addTarget(scr, laser, target, r,  )
+            simImg   = addTarget(scr, laser, target, r, angle)
             cv2.imwrite(ansPath + filename, simImg)
     '''
     cv2.imwrite(ansPath, simImg)
@@ -53,10 +53,10 @@ def addTarget(imgScr, laser, target, r, angle):
     startRow  = int(min(max(imgCenRow - r * math.sin((angle / 180) * math.pi) - sptCenRow, 0), imgRow - sptRow))
     startCol  = int(min(max(imgCenCol + r * math.cos((angle / 180) * math.pi) - sptCenCol, 0), imgCol - sptCol))
     imgAns = np.array(imgScr)
-    for row in range(0, imgRow):
+    '''for row in range(0, imgRow):
         for col in range(0, imgCol):
             gay = int(imgScr[row][col]) + int(laserD[row][col])
-            imgAns[row][col] = min(gay, int(255))
+            imgAns[row][col] = min(gay, int(255))'''
     for row in range(0, sptRow):
         for col in range(0, sptCol):
             gay = int(target[row][col]) + int(imgAns[startRow + row][startCol + col])
