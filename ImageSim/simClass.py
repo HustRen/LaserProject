a394777a5d6b477ba677236e263f239b681c5fba
ren <hust_ren@hust.edu.cn>
2018-11-30 11:17:02 +0800

    利用光斑数学模型，新增LaserSpot光斑仿真类

--- ImageSim/simClass.py
+++ ImageSim/simClass.py
@@ -29,6 +29,35 @@ class Noise(SimAlgorSuper):
                 self._data[row][col] = min(gay, int(255))
         return self._data
 
+class LaserSpot(SimAlgorSuper):
+    def __init__(self, im_size, position, radius):
+        super().__init__(im_size)
+        self.__spotRow = position[0] if position[0] >= 0 else 0
+        self.__spotCol = position[1] if position[1] >= 0 else 0
+        self.__mainSpotRadius = radius
+        self.__secSpotPara_A = 267.8
+        self.__secSpotPara_a = -0.707
+        self.__secSpotPara_b = -0.7621
+    
+    def data(self):
+        rowUp = min(self.__spotRow + self.__mainSpotRadius + 1, self._data.shape[0])
+        rowDown = max(self.__spotRow - self.__mainSpotRadius, 0)
+        colUp = min(self.__spotCol + self.__mainSpotRadius + 1, self._data.shape[1])
+        colDown = max(0, self.__spotCol - self.__mainSpotRadius)
+        for row in range(rowDown, rowUp):
+            for col in range(colDown, colUp):
+                d = np.sqrt(pow(row - self.__spotRow, 2) + pow(col - self.__spotCol, 2))
+                if d - self.__mainSpotRadius < 0.51:
+                    self._data[row][col] = 255
+        for row in range(0, self._data.shape[0]):
+            for col in range(0, self._data.shape[1]):
+                d = np.sqrt(pow(row - self.__spotRow, 2) + pow(col - self.__spotCol, 2))
+                if d > self.__mainSpotRadius:
+                    index = 2 / (1 + np.exp(pow(self.__mainSpotRadius, -0.7621) * (d - self.__mainSpotRadius)))
+                    self._data[row][col] = int(255 * index)
+        return self._data
+    
+
 class Target(SimAlgorSuper):
     def __init__(self, im_size, target, r, angle):
         super().__init__(im_size)
@@ -91,7 +120,8 @@ def main():
     im = SimImage(scr.shape)
     im.add(Background(scr.shape, scr))
     #im.add(Noise(scr.shape, laser))
-    for angle in range(0, 360, 60):
+    im.add(LaserSpot(scr.shape, (100, 200), 60))
+    for angle in range(300, 360, 60):
         for r in range(20, 120, 100):
             im.add(Target(scr.shape, target, r, angle))
 
