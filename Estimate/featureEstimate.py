29042535027f1251a7a3f8cd125e47b25a2df92a
ren <hust_ren@hust.edu.cn>
2018-12-27 15:22:30 +0800

    secondySpot.py改名为featureEstimate.py

--- Estimate/featureEstimate.py
+++ Estimate/featureEstimate.py
@@ -17,12 +17,16 @@ def detect_circles_demo(image):
     dst = cv2.pyrMeanShiftFiltering(image, 10, 100)   #边缘保留滤波EPF
     cv2.imshow('test', dst)
     cimage = cv2.cvtColor(dst, cv2.COLOR_RGB2GRAY)
-    circles = cv2.HoughCircles(cimage, cv2.HOUGH_GRADIENT, 1, 20, param1=60, param2=20, minRadius=10, maxRadius=100)
-    for i in circles[0, : ]:
-        i = np.uint16(np.around(i)) #把circles包含的圆心和半径的值变成整数
-        cv2.circle(image, (i[0], i[1]), i[2], (0, 0, 255), 2)  #画圆
-        cv2.circle(image, (i[0], i[1]), 2, (0, 0, 255), 2)  #画圆心
+    circles = cv2.HoughCircles(cimage, cv2.HOUGH_GRADIENT, 1, 20, param1=60, param2=20, minRadius=5, maxRadius=100)
+    if type(circles) != None:
+        for i in circles[0, : ]:
+            i = np.uint16(np.around(i)) #把circles包含的圆心和半径的值变成整数
+            #if image[i[0]][i[1]][0] > 200:
+            cv2.circle(image, (i[0], i[1]), i[2], (0, 0, 255), 2)  #画圆
+            cv2.circle(image, (i[0], i[1]), 2, (0, 0, 255), 2)  #画圆心
     cv2.imshow("circles", image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
 
 class SpotEstimate():
@@ -39,20 +43,27 @@ class SpotEstimate():
         return eImage.textureSynthesis()
 
     @staticmethod
-    def getMask(occlusionImage):
-        image = cv2.cvtColor(occlusionImage, cv2.COLOR_GRAY2RGB)
-        dst = cv2.pyrMeanShiftFiltering(image, 10, 100)
-        cimage = cv2.cvtColor(dst, cv2.COLOR_RGB2GRAY)
-        circles = cv2.HoughCircles(cimage, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0) 
-        circle = circles[0, 0, :]
-        mask3 = np.zeros(image.shape, dtype=np.uint8)
-        cv2.circle(mask3, (circle[0], circle[1]), circle[2], (255,255,255), thickness=cv2.FILLED)
-        mask = cv2.cvtColor(mask3, cv2.COLOR_RGB2GRAY)
+    def getMask(occlusionImage, dtype = 'circles'):
+        if dtype == 'circles':
+            image = cv2.cvtColor(occlusionImage, cv2.COLOR_GRAY2RGB)
+            dst = cv2.pyrMeanShiftFiltering(image, 10, 100)
+            cimage = cv2.cvtColor(dst, cv2.COLOR_RGB2GRAY)
+            circles = cv2.HoughCircles(cimage, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0) 
+            circle = circles[0, 0, :]
+            mask3 = np.zeros(image.shape, dtype=np.uint8)
+            cv2.circle(mask3, (circle[0], circle[1]), circle[2], (255,255,255), thickness=cv2.FILLED)
+            mask = cv2.cvtColor(mask3, cv2.COLOR_RGB2GRAY)
+        else:
+            rStr, rowStr, colStr=dtype.split('_')
+            image = cv2.cvtColor(occlusionImage, cv2.COLOR_GRAY2RGB)
+            mask3 = np.zeros(image.shape, dtype=np.uint8)
+            cv2.circle(mask3, (int(rStr), int(colStr)), rStr + 2, (255,255,255), thickness=cv2.FILLED)
+            mask = cv2.cvtColor(mask3, cv2.COLOR_RGB2GRAY)
         return mask
 
 class VarMeanEstimate(object):
-    def __init__(self, image, grid):
-        mask = SpotEstimate.getMask(image)
+    def __init__(self, image, grid, dtype = 'circles'):
+        mask = SpotEstimate.getMask(image, 'circles')
         self.gridImage = GridImage(image, grid)
         size = self.gridImage.meanImage.shape
         scaleMask = cv2.resize(mask, (size[1], size[0]), interpolation=cv2.INTER_NEAREST)
@@ -84,9 +95,15 @@ class VarMeanEstimate(object):
 def estimateFeature(filePath):
     image = cv2.imread(filePath, cv2.IMREAD_GRAYSCALE)
 
+def getfilename(fullpath):
+    tname = os.path.basename(fullpath) 
+    (shotname,extension) = os.path.splitext(tname)#文件名、后缀名
+    return shotname
 
 if __name__ == "__main__":
-    '''scr = cv2.imread('D:/LaserData/ans/level0/level0_0_0.png')
+    '''scr = cv2.imread('D:/LaserData/background/1024X1024/sim/P0783__1__0___0/50_416_72.png', cv2.IMREAD_GRAYSCALE)
+    scr[scr < 255] = 0
+    scr = np.repeat(scr[:,:,np.newaxis], 3, axis=2)
     detect_circles_demo(scr)
     cv2.waitKey()
     cv2.destroyAllWindows()'''
@@ -124,24 +141,6 @@ if __name__ == "__main__":
     print('gird scrImage and disImage SSIM: %f', (WMSSIM.SSIM(gridScr.varImage, gridDist.varImage)))
     print('gird estImage and disImage SSIM: %f', (WMSSIM.SSIM(vm.varImg, gridDist.varImage)))
 
-    '''gridScr = GridImage(scrImage, 10)
-    mask = SpotEstimate.getMask(cv2.imread('D:/LaserData/plane/estimate/occlusion.png', cv2.IMREAD_GRAYSCALE))
-    gridMask = GridImage(mask, 10)
-    gridOcc = GridImage(cv2.imread('D:/LaserData/plane/estimate/occlusion.png', cv2.IMREAD_GRAYSCALE), 10)
-    occEst = OcclusionEstimateImage(gridOcc.meanImage, gridMask.meanImage, 3)
-    occEst.debugModel('D:/LaserData/plane/estimate/Grid')
-    gridEst = occEst.textureSynthesis()
-
-    plt.subplot(1,2,1)
-    plt.title('scr')
-    plt.imshow(gridScr)
-
-    plt.subplot(1,2,2)
-    plt.title('Est')
-    plt.imshow(gridEst)
-
-    plt.show()
-    plt.waitforbuttonpress()'''
 
     #g = GridImage(maskImage, 10)
     #g.show('mean')
